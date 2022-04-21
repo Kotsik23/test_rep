b960edbe88cb0067181f16c9cabc0ac28d4ff2c2
oleg <olegsmushko@gami.com>
2022-04-21 18:19:23 +0300

    Add: text execution task

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 function btnClick() {
     alert("Hello, Human 🤚");
 }
+
+const deleteLetter = (word, deleteLetter) => {
+    let result = "";
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] !== deleteLetter) {
+            result += word[i];
+        }
+    }
+    return result;
+}
+
+const performTask = () => {
+    let word = document.getElementById("user-word").value;
+    let letter = document.getElementById("user-letter").value;
+
+    if (!word || !letter) {
+        alert("Incorrect values.");
+        return;
+    }
+
+    let result = deleteLetter(word, letter);
+    alert(result);
+}
